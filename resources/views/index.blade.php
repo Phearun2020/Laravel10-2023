b6a9e6b1119f7aa4e817caf48abd15497e140a80
Phearun2020 <phearun.ph77@gmail.com>
2023-09-09 14:59:44 +0700

    :memo: For and for loop
    using if else loop ...

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,3 +8,23 @@
     The name is: {{ $name }}
 @endisset
 
+<h1>The list of tasks</h1>
+<div>
+    @if (count($tasks))
+        @foreach ($tasks as $task)
+            <div>{{ $task->title}}</div>
+        @endforeach
+    @else
+        <div>There are no tasks!</div>
+    @endif
+    {{-- we can use loop --}}
+    @forelse ($tasks as $task)
+    <div>
+        <a href="{{ route('tasks.show', ['id' => $task->id])}}">{{ $task->title }}</a>
+    </div>
+
+    @empty
+        <div>There are no task</div>
+    @endforelse
+</div>
+
